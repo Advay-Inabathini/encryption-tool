4dc2e819028608dd95d6cf4df0e6a8391be299f9
Advay-Inabathini <53119893+Advay-Inabathini@users.noreply.github.com>
2024-02-14 13:30:05 +0530

    Update pointdoubling.py
    better time measure of program

--- pointdoubling.py
+++ pointdoubling.py
@@ -4,13 +4,14 @@ def mod_Inv(x,y):
     for i in range(y):
         if (x*i)%y==1:
             return i
-start = time.time()
+
 p = int(input("enter p of finite field: "))
 a = int(input("enter coefficient of curve equation(a): "))
 print("\n")
 x1 = int(input("enter x coordinate of given point: "))
 y1 = int(input("enter y coordinate of given point: "))
 
+start = time.time()
 lnum = (3*(x1**2)+a)%p
 ldenom = mod_Inv(2*y1,p)
 
